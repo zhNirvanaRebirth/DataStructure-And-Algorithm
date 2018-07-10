0c422b47cb08410fa3db5d33a2c6a468226ac072
zh <zh@com.centos.zhwilson>
2018-07-10 04:58:40 -0400

    array:洗牌问题，素数问题

---
+++ array_probe.c
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+//去掉极值求平均分
+//问题：如果是int数据，求和可能造成数据溢出，有没有其他方式？
+//时间复杂度：O(n)
+float average(float* brr, int length) {
+	if (length < 3) {
+		printf("the length of the array must bigger than 2!!");
+		return 0.0f;
+	}
+	int i;
+	float max = brr[0];
+	float min = brr[0];
+	float sum = brr[0];
+	for (i = 1; i < length; i++) {
+		if (brr[i] > max) max = brr[i];
+		if (brr[i] < min) max = brr[i];
+		sum += brr[i];
+	}
+	return (sum - max - min)/(length - 2);
+}
+//计算π的值
+//问题1：随着计算精确度的提高，需要的计算步数会增加，那么有没有更好的方法，控制需要进行的计算步数，以减小计算的时间复杂度？（能不能用香克斯变换？）
+//问题2：怎么去判断这个精确度满足条件了，就是下面这个while的终止条件是什么？
+//公式：π/4 = 1 - 1/3 + 1/5 - 1/7 + 1/9 - ....
+double get_pi(int accuracy) {
+	int i = 1;
+	double pi = 0;
+	double acc = pow(10, accuracy);
+	int result =(int) (M_PI/4 * acc);
+	int temp = 0;
+	while (result != temp) {//这个终止条件目前来看是有问题的！！
+		if (i%2 != 0) pi = pi + 1.0/(2*i - 1);
+		else pi = pi - 1.0/(2*i - 1);
+		i++;
+		temp =(int) (pi*acc);
+	}
+	printf("with accuracy: %d need %d steps\n", accuracy, i);
+}
+
+//9*9乘法表
+void multiplication_table() {
+	int i, j;
+	for (i = 1; i <= 9; i++){
+		for (j = 1; j <= i; j++) {
+			printf("%d*%d=%d\t", j, i, i*j);
+		}
+		printf("\n");
+	}
+	return;
+}
+
+//洗牌问题
+void shuffle(int* brr, int length) {
+	int i, temp;
+	srand((int)time(0));
+	for (i = 0; i < length - 1; i++) {
+		int index = i + (int)(1.0f*(length - i)*rand()/RAND_MAX);
+		//printf("i-index:%d-%d\t", i, index);
+		if (i == index) continue;
+		temp = brr[i];
+		brr[i] = brr[index];
+		brr[index] = temp;		
+	}
+} 
+
+//计算素数
+//问题：如何计算素数，如何查找小于某个数N的素数？有什么方法，每种方法的时间复杂度？
+//TODO
+
+
+void show_array(int* crr, int length){
+	int i;
+	for (i = 0; i < length; i++) {
+		printf("%d ", crr[i]);
+	}
+	printf("\n");
+}
+int main(void) {
+	/*
+	printf("the average value:\n");
+	float arr[] = {10.2f, 49.0f, 26.56f, 32.33f};
+	float avg = average(arr, 4);
+	printf("%.4f\n", avg);
+	*/
+
+	/*
+	get_pi(7);
+	*/
+
+	/*
+	multiplication_table();
+	*/
+	
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+	shuffle(arr, 12);
+	show_array(arr, 12);
+
+	return 0;
+}
+
+
